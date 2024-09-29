a65d6e60e9a8082e94503b0b93fb514e5168aede
Adam Klein <adam3207@gmail.com>
2024-09-29 03:02:43 -0700

    zlm annealed

--- src/trainers/xla_zlm_trainer.py
+++ src/trainers/xla_zlm_trainer.py
@@ -14,22 +14,22 @@ from utils.training_utils import (
 class XLAZLmTrainer(BaseXLATrainer):
 
     def train_step(self, step, model, x):
-        kl_min = self.anneal_max * max(0, 1 - step/self.anneal_steps)
 
         logits, kl = model(x)
         
         # shift kl
         kl = kl[:, :-1].mean()
-        kl_clip = kl < kl_min
 
         results = DotDict(
             nlogp=loss(logits, x),
             acc=acc(logits, x),
             pcorr=pcorr(logits, x),
             kl=kl,
-            kl_min=torch.full_like(kl, kl_min),
-            kl_clip=kl_clip.float()
         )
+
+        results.kl_thresh = (results.nlogp * max(0, 1 - step/self.anneal_steps)).detach()
+        kl_clip = kl < results.kl_thresh
+        results.kl_clip = kl_clip.float()
 
         results.nelbo = results.nlogp + kl
         results.loss = (
