fa761912f05297425aa9a4edf09034285efa5529
Adam Klein <adam3207@gmail.com>
2024-09-28 18:20:50 -0700

    med patch hlm

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,18 +1,17 @@
 """ Models """
 
-# from models.base import BaseConfig, BaseLmModel
-# from models.hlm import HLmConfig, HLmModel
-# from models.patch_hlm import PatchHLmConfig, PatchHLmModel
+from models.hlm import HLmConfig, HLmModel
+from models.patch_hlm import PatchHLmConfig, PatchHLmModel
 from models.zlm import ZLmConfig, ZLmModel
 
 CONFIG_DICT = {
-    # "hlm": HLmConfig,
-    # "patch_hlm": PatchHLmConfig,
+    "hlm": HLmConfig,
+    "patch_hlm": PatchHLmConfig,
     "zlm": ZLmConfig,
 }
 
 MODEL_DICT = {
-    # "hlm": HLmModel,
-    # "patch_hlm": PatchHLmModel,
+    "hlm": HLmModel,
+    "patch_hlm": PatchHLmModel,
     "zlm": ZLmModel,
 }
