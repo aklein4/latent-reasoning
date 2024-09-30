006175d11a8b12bf971e3b282fddfb52194e9aea
Adam Klein <adam3207@gmail.com>
2024-09-29 23:58:50 -0700

    deprecatoin

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,18 +3,15 @@
 from models.hlm import HLmConfig, HLmModel
 from models.patch_hlm import PatchHLmConfig, PatchHLmModel
 from models.zlm import ZLmConfig, ZLmModel
-from models.markovlm import MarkovLmConfig, MarkovLmModel
 
 CONFIG_DICT = {
     "hlm": HLmConfig,
     "patch_hlm": PatchHLmConfig,
     "zlm": ZLmConfig,
-    "markovlm": MarkovLmConfig,
 }
 
 MODEL_DICT = {
     "hlm": HLmModel,
     "patch_hlm": PatchHLmModel,
     "zlm": ZLmModel,
-    "markovlm": MarkovLmModel,
 }
--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -2,10 +2,8 @@
 
 from trainers.xla_hlm_trainer import XLAHLmTrainer
 from trainers.xla_zlm_trainer import XLAZLmTrainer
-from trainers.xla_markovlm_trainer import XLAMarkovLmTrainer
 
 TRAINER_DICT = {
     "XLAHLmTrainer": XLAHLmTrainer,
     "XLAZLmTrainer": XLAZLmTrainer,
-    "XLAMarkovLmTrainer": XLAMarkovLmTrainer
 }
