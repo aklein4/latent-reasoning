bc3d7294439999928813f2bc1f40a8561a87ee88
Adam Klein <adam3207@gmail.com>
2024-10-02 03:49:23 -0700

    deprecate zlm

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,16 +2,13 @@
 
 from models.hlm import HLmConfig, HLmModel
 from models.patch_hlm import PatchHLmConfig, PatchHLmModel
-from models.zlm import ZLmConfig, ZLmModel
 
 CONFIG_DICT = {
     "hlm": HLmConfig,
     "patch_hlm": PatchHLmConfig,
-    "zlm": ZLmConfig,
 }
 
 MODEL_DICT = {
     "hlm": HLmModel,
     "patch_hlm": PatchHLmModel,
-    "zlm": ZLmModel,
 }
--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -1,9 +1,7 @@
 """ Training package """
 
 from trainers.xla_hlm_trainer import XLAHLmTrainer
-from trainers.xla_zlm_trainer import XLAZLmTrainer
 
 TRAINER_DICT = {
     "XLAHLmTrainer": XLAHLmTrainer,
-    "XLAZLmTrainer": XLAZLmTrainer,
 }
