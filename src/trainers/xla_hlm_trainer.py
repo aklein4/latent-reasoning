b681e1f8a5b98def39a027fac82f837feb19d71a
Adam Klein <adam3207@gmail.com>
2024-10-03 14:17:36 -0700

    modernized hlm trainer

--- src/trainers/xla_hlm_trainer.py
+++ src/trainers/xla_hlm_trainer.py
@@ -19,21 +19,27 @@ class XLAHLmTrainer(BaseXLATrainer):
         )
         return -clipped_log_probs.mean()
 
-    def kl_loss(self, kl):
+    def kl_loss(self, kl, smooth=False):
         # return kl with every token weighted equally
+        if not smooth:
+            kl = kl.sum(-1)
         return kl.mean()
 
-    def loss(self, token_loss, kl_loss, kl_clip):
+    def loss(self, token_loss, kl_loss, kl_smooth_loss, kl_entropy):
         # if either clip triggers, only use kl
         return (
             self.kl_w * kl_loss +
-            self.token_w * torch.where(
-                kl_clip,
-                token_loss.detach(),
-                token_loss,
-            )
+            self.kl_smooth_w * kl_smooth_loss -
+            self.kl_entropy_w * kl_entropy +
+            self.token_w * token_loss
         )
     
+
+    def kl_entropy(self, kl):
+        avg = kl.mean(1).mean(0)
+        probs = avg / avg.sum()
+        return -(probs * torch.log(probs)).sum()
+
 
     def acc(self, logits, x, mask):
         correct = (logits.argmax(-1) == x).float()
@@ -52,26 +58,21 @@ class XLAHLmTrainer(BaseXLATrainer):
         return -log_probs.sum() / (mask & (x != pad)).float().sum()
 
 
-    def kl_per_token(self, kl, mask):
-        assert kl.shape == mask.shape
+    def kl_per_token(self, kl, mask, smooth=False):
+        if not smooth:
+            kl = kl.sum(-1)
         return kl.sum() / mask.float().sum()
 
-    def kl_per_token_nopad(self, kl, mask, x, pad):
-        assert kl.shape == mask.shape
-        assert kl.shape == x.shape
+    def kl_per_token_nopad(self, kl, mask, x, pad, smooth=False):
+        if not smooth:
+            kl = kl.sum(-1)
         return kl.sum() / (mask & (x != pad)).float().sum()
 
 
     def train_step(self, step, model, x, mask):
         bs, seq_len = x.shape
-        
-        uncond_x = x[:self.num_uncond]
-        cond_x = x[self.num_uncond:]
-        uncond_mask = mask[:self.num_uncond]
-        cond_mask = mask[self.num_uncond:]
 
-        logits, uncond_kl, cond_kl = model(x, mask, num_uncond=self.num_uncond)
-        kl = torch.cat([uncond_kl, cond_kl], dim=0)
+        logits, kl, smooth_kl = model(x, mask)
 
         # log probs, with zero for unmasked tokens
         ar = torch.arange(x.numel(), device=x.device, dtype=x.dtype)
@@ -81,28 +82,33 @@ class XLAHLmTrainer(BaseXLATrainer):
         log_probs = torch.where(mask, log_probs, torch.zeros_like(log_probs))
 
         # current version is raw logit clipping
-        kl_clip = self.kl_per_token(cond_kl, cond_mask) > self.kl_threshold
         clip_mask = mask & (log_probs < np.log(self.clip_prob))  # (torch.argmax(logits, dim=-1) != x)
 
         results = DotDict(
             token_loss=self.token_loss(log_probs, clip_mask),
             kl_loss=self.kl_loss(kl),
+            kl_smooth_loss=self.kl_loss(smooth_kl, smooth=True),
             
+            kl_entropy=self.kl_entropy(kl),
+
             acc=self.acc(logits, x, mask),
             clip_perc=self.clip_perc(mask, clip_mask),
             
             logp_per_token=self.logp_per_token(log_probs, mask),
             logp_per_token_nopad=self.logp_per_token_nopad(log_probs, mask, x, model.config.pad_token_id),
             
-            kl_per_token=self.kl_per_token(cond_kl, cond_mask),
-            kl_per_token_nopad=self.kl_per_token_nopad(cond_kl, cond_mask, cond_x, model.config.pad_token_id),
+            kl_per_token=self.kl_per_token(kl, mask),
+            kl_per_token_nopad=self.kl_per_token_nopad(kl, mask, x, model.config.pad_token_id),
             
-            uncond_kl_per_token=self.kl_per_token(uncond_kl, uncond_mask),
-            uncond_kl_per_token_nopad=self.kl_per_token_nopad(uncond_kl, uncond_mask, uncond_x, model.config.pad_token_id),
-            
-            kl_clip=kl_clip.float(),
+            kl_smooth_per_token=self.kl_per_token(smooth_kl, mask, smooth=True),
+            kl_smooth_per_token_nopad=self.kl_per_token_nopad(smooth_kl, mask, x, model.config.pad_token_id, smooth=True),
         )
-        results.loss = self.loss(results.token_loss, results.kl_loss, kl_clip)
+        results.loss = self.loss(
+            results.token_loss,
+            results.kl_loss,
+            results.kl_smooth_loss,
+            results.kl_entropy
+        )
 
         results.one_minus_acc = 1 - results.acc
         results.one_minus_clip_perc = 1 - results.clip_perc
