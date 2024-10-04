26710b9149b3aba64190a22f7f0a8bc129df1d43
Adam Klein <adam3207@gmail.com>
2024-10-04 05:52:07 -0700

    barriers

--- src/train_xla.py
+++ src/train_xla.py
@@ -38,6 +38,7 @@ def _mp_fn(index, args):
     model_type_config = CONFIG_DICT[model_type](**model_config)
     model = MODEL_DICT[model_type](model_type_config)
     model = model.init_fsdp()
+    xm.mark_step()
     xm.rendezvous("Model Loaded!")
     log_master_print("Model Loaded!")
 
