828467fc6c7bd0a62fb08ca4be9512cdbc308dff
Adam Klein <adam3207@gmail.com>
2024-10-03 23:18:09 -0700

    memory

--- src/trainers/base_xla_trainer.py
+++ src/trainers/base_xla_trainer.py
@@ -242,8 +242,7 @@ class BaseXLATrainer:
                 results.loss.backward()
 
                 # mark step if using gradient accumulation
-                if constants.XLA_MAIN():
-                    xm.get_memory_info(constants.XLA_DEVICE())
+                log_master_print(xm.get_memory_info(constants.XLA_DEVICE()))
                 if num_mini_batches > 1 and mini_batch_id < num_mini_batches - 1:
                     xm.mark_step()
 
