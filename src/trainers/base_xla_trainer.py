40be1f18ca930ec4d610e802597ba24701e9f522
Adam Klein <adam3207@gmail.com>
2024-10-05 01:35:55 -0700

    remove fsdp

--- src/trainers/base_xla_trainer.py
+++ src/trainers/base_xla_trainer.py
@@ -14,6 +14,7 @@ import huggingface_hub as hf
 
 import utils.constants as constants
 from utils.data_utils import DotDict
+from utils.optimization_utils import LowPrecisionAdafactor
 from utils.logging_utils import LogSection, log_print, log_master_print
 
 
@@ -134,10 +135,14 @@ class BaseXLATrainer:
         
 
     def get_optimizer(self, model):
-        return syncfree.AdamW(
+        return LowPrecisionAdafactor(
             model.parameters(), lr=self.start_lr,
             **self.optimizer_kwargs
         )
+        # return syncfree.AdamW(
+        #     model.parameters(), lr=self.start_lr,
+        #     **self.optimizer_kwargs
+        # )
 
 
     def get_scheduler(self, optimizer):
@@ -218,12 +223,12 @@ class BaseXLATrainer:
                 # get results from train step
                 
                 # fsdp handles autocast
-                # with autocast(constants.XLA_DEVICE()):
-                results = self.train_step(
-                    curr_step,
-                    model,
-                    *mini_batch
-                )
+                with autocast(constants.XLA_DEVICE()):
+                    results = self.train_step(
+                        curr_step,
+                        model,
+                        *mini_batch
+                    )
 
                 # scale results for accumulation
                 # reductions are done by averaging across devices, summing across mini batches
@@ -245,10 +250,11 @@ class BaseXLATrainer:
                     xm.mark_step()
 
             # perform a single optimizer step
-            if self.clip_grad_norm is not None:
-                model.clip_grad_norm_(self.clip_grad_norm)
-            optimizer.step()
-            optimizer.zero_grad(set_to_none=(num_mini_batches == 1))
+            # if self.clip_grad_norm is not None:
+            #     model.clip_grad_norm_(self.clip_grad_norm)
+            xm.optimizer_step(optimizer)
+            # optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
 
             # update lr
             self.log.lr = lr_scheduler.get_last_lr()[0]
