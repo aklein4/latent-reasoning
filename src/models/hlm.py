0932e8a909f1d22c5b56e4f412f7f9241e012d5d
Adam Klein <adam3207@gmail.com>
2024-10-05 21:21:23 -0700

    unflatten whole model

--- src/models/hlm.py
+++ src/models/hlm.py
@@ -738,9 +738,9 @@ class HLmModel(XLAModel):
 
 
     def init_fsdp(self):
-        self.encoder.init_fsdp()
-        self.generator.init_fsdp()
-        self.decoder.init_fsdp()
+        # self.encoder.init_fsdp()
+        # self.generator.init_fsdp()
+        # self.decoder.init_fsdp()
 
         return apply_fsdp(self, False, self.config.reshard_after_forward)
 
