a1b595299524421d742448ed8d21c44ae81096e0
Adam Klein <adam3207@gmail.com>
2025-04-11 07:04:57 +0000

    uncond hook

--- src/trainers/uncond_zlm_trainer.py
+++ src/trainers/uncond_zlm_trainer.py
@@ -12,6 +12,9 @@ from utils.dot_dict import DotDict
 
 class UncondZLmTrainer(BaseTrainer):
     
+    hooked = False
+
+
     def train_step(self, step, model, input_ids, output_ids):
 
         # get model predictions
@@ -24,13 +27,6 @@ class UncondZLmTrainer(BaseTrainer):
             dim=-1,
         )[..., 0]
 
-        kl = (output.encoder_mus - output.generator_mus).pow(2).sum(-1) / 2
-
-        mean_mus = output.encoder_mus.mean(0, keepdim=True)
-        mean_kl = (
-            output.encoder_mus - mean_mus
-        ).pow(2).sum(-1).detach() / 2
-
         # calculate lm metrics
         results = DotDict(
             lm_loss = -logp.mean(),
@@ -38,7 +34,23 @@ class UncondZLmTrainer(BaseTrainer):
             lm_acc = (output.lm_logits.argmax(-1) == output_ids).float().mean(),
         )
         
+        # handle hook
+        if not self.hooked:
+            if results.lm_acc.item() >= self.acc_hook:
+                self.hooked = True
+                results.reset_optimizer = 1.0
+
         # calculate kl metrics
+        kl = (
+            (output.encoder_mus if self.hooked else output.encoder_mus.detach()) -
+            output.generator_mus
+        ).pow(2).sum(-1) / 2
+
+        mean_mus = output.encoder_mus.mean(0, keepdim=True)
+        mean_kl = (
+            output.encoder_mus - mean_mus
+        ).pow(2).sum(-1).detach() / 2
+
         results.kl_per_channel = kl.mean() / model.latent_size_per_layer
         results.kl_per_token = kl.mean() * (model.z_length * model.num_latent_layers) / model.output_length
         results.kl_per_token_mean = mean_kl.mean() * (model.z_length * model.num_latent_layers) / model.output_length
@@ -53,18 +65,9 @@ class UncondZLmTrainer(BaseTrainer):
         results.lm_loss_scaled = -(logp * results.lm_scale).mean()
 
         # get weighted kl
-        results.kl_scale = 1e-7 + torch.clip(
-            (results.kl_per_token - self.lower_kl_bound) / (self.upper_kl_bound - self.lower_kl_bound),
-            min=0.0,
-            max=1.0,
-        ).detach().item()
-
-        kl = (
-            scale_gradient(output.encoder_mus, results.kl_scale) - output.generator_mus
-        ).pow(2).sum(-1) / 2
-
         seq_kl = kl.reshape(-1, kl.shape[-2] * kl.shape[-1]).mean(0)
         weighted_kl = seq_kl * (seq_kl / (seq_kl.mean() + 1e-7)).detach()
+        
         results.kl_per_token_weighted = weighted_kl.sum() / model.output_length
         results.kl_per_token_weighted_scaled = self.kl_scale * results.kl_per_token_weighted
 
