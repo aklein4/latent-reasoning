0abfd3c944e91e65012a81c621d6793a72a2e3aa
Adam Klein <adam3207@gmail.com>
2025-05-01 02:03:37 +0000

    zero optimize

--- src/trainers/zlm_full_trainer.py
+++ src/trainers/zlm_full_trainer.py
@@ -14,6 +14,7 @@ class ZLmFullTrainer(BaseTrainer):
     
     running_kls_per_channel = None
     running_mean_kls_per_channel = None
+    running_zeros_per_channel = None
 
     hooked = False
     hooked_steps = 0
@@ -29,7 +30,7 @@ class ZLmFullTrainer(BaseTrainer):
     def train_step(self, step, model, input_ids, output_ids):
 
         # get model predictions
-        model_out = model(input_ids, output_ids, warming=(not self.hooked))
+        model_out = model(input_ids, output_ids, disable_generator=(not self.hooked))
 
         # extract probabilities
         logp = torch.take_along_dim(
@@ -83,75 +84,91 @@ class ZLmFullTrainer(BaseTrainer):
             model_out.encoder_mus - model_out.encoder_mus.mean(0, keepdim=True)
         ).pow(2).sum(-2) / 2
 
+        zero_kl = (
+            model_out.encoder_mus - torch.zeros_like(model_out.encoder_mus)
+        ).pow(2).sum(-2) / 2
+
         results.kl_per_token = (kl.mean(0).sum() / model.output_length)
         results.kl_per_channel = (kl.mean() / model.latent_size_per_layer)
         
         results.mean_kl_per_token = (mean_kl.mean(0).sum() / model.output_length)
         results.mean_kl_per_channel = (mean_kl.mean() / model.latent_size_per_layer)
 
+        results.zero_kl_per_token = (zero_kl.mean(0).sum() / model.output_length)
+        results.zero_kl_per_channel = (zero_kl.mean() / model.latent_size_per_layer)
+
         results.elbo = results.lm_loss + results.kl_per_token
 
         # save the running metrics
         kl_per_channel_mean = (kl.mean(0) / model.latent_size_per_layer).detach().clone().float()
         mean_kl_per_channel_mean = (mean_kl.mean(0) / model.latent_size_per_layer).detach().clone().float()
+        zero_kl_per_channel_mean = (zero_kl.mean(0) / model.latent_size_per_layer).detach().clone().float()
 
         if self.running_kls_per_channel is None:
             self.running_kls_per_channel = kl_per_channel_mean
             self.running_mean_kls_per_channel = mean_kl_per_channel_mean
+            self.running_zeros_per_channel = zero_kl_per_channel_mean
         else:
             self.running_kls_per_channel = self.running_kls_per_channel * self.running_beta + kl_per_channel_mean * (1 - self.running_beta)
             self.running_mean_kls_per_channel = self.running_mean_kls_per_channel * self.running_beta + mean_kl_per_channel_mean * (1 - self.running_beta)
+            self.running_zeros_per_channel = self.running_zeros_per_channel * self.running_beta + zero_kl_per_channel_mean * (1 - self.running_beta)
 
-        # balance the hidden kl weights
+        # balance the kl weights
         normalized_kl_weights = self.running_kls_per_channel / (self.running_kls_per_channel.max() + 1e-7)
-        clipped_kl_weights = torch.where(
-            normalized_kl_weights > self.kl_weight_threshold,
-            normalized_kl_weights,
-            0.0,
+        clipped_kl_weights = torch.clip(
+            normalized_kl_weights - self.kl_weight_threshold,
+            min=0.0,
         )
         kl_val_per_token = (kl.mean(0) * clipped_kl_weights).sum() / model.output_length
         kl_val_multiplier = results.kl_per_token / (1e-7 + kl_val_per_token)
         final_kl_weights = clipped_kl_weights * kl_val_multiplier.detach().item()
 
         kl_to_loss = (
-            scale_gradient(model_out.encoder_mus, final_kl_weights[None, :, None, :].detach()) -
+            scale_gradient(
+                model_out.encoder_mus.detach() if self.zero_optimize else model_out.encoder_mus,
+                final_kl_weights[None, :, None, :].detach()
+            ) -
             model_out.generator_mus
         ).pow(2).sum(-2) / 2
         results.kl_per_token_weighted = kl_to_loss.mean(0).sum() / model.output_length
 
-        # balance the mean kl weights
-        normalized_mean_kl_weights = self.running_mean_kls_per_channel / (self.running_mean_kls_per_channel.max() + 1e-7)
-        clipped_mean_kl_weights = torch.where(
-            normalized_mean_kl_weights > self.kl_weight_threshold,
-            normalized_mean_kl_weights,
-            0.0,
+        # balance the zero kl weights
+        normalized_zero_kl_weights = self.running_zeros_per_channel / (self.running_zeros_per_channel.max() + 1e-7)
+        clipped_zero_kl_weights = torch.clip(
+            normalized_zero_kl_weights - self.kl_weight_threshold,
+            min=0.0,
         )
-        mean_kl_val_per_token = (mean_kl.mean(0) * clipped_mean_kl_weights).sum() / model.output_length
-        mean_kl_val_multiplier = results.mean_kl_per_token / (1e-7 + mean_kl_val_per_token)
-        final_mean_kl_weights = clipped_mean_kl_weights * mean_kl_val_multiplier.detach().item()
+        zero_kl_val_per_token = (zero_kl.mean(0) * clipped_zero_kl_weights).sum() / model.output_length
+        zero_kl_val_multiplier = results.zero_kl_per_token / (1e-7 + zero_kl_val_per_token)
+        final_zero_kl_weights = clipped_zero_kl_weights * zero_kl_val_multiplier.detach().item()
 
-        mean_kl_to_loss = mean_kl * final_mean_kl_weights[None].detach()
-        results.mean_kl_per_token_weighted = mean_kl_to_loss.mean(0).sum() / model.output_length
+        zero_kl_to_loss = (
+            scale_gradient(
+                model_out.encoder_mus if self.zero_optimize else model_out.encoder_mus.detach(),
+                final_zero_kl_weights[None, :, None, :].detach()
+            ) -
+            torch.zeros_like(model_out.encoder_mus)
+        ).pow(2).sum(-2) / 2
+        results.zero_kl_per_token_weighted = zero_kl_to_loss.mean(0).sum() / model.output_length
 
+
+        results.kl_loss = results.kl_per_token_weighted + results.zero_kl_per_token_weighted
         results.kl_scale = self.kl_scale * min(1.0, self.hooked_steps / self.hook_warmup_steps)
-        results.kl_balance = min(1.0, self.hooked_steps / self.mean_warmup_steps)
-
-        results.kl_loss = (
-            (1e-7 + 1 - results.kl_balance) * results.mean_kl_per_token_weighted +
-            results.kl_balance * results.kl_per_token_weighted
-        )
 
         results.loss = (
             results.lm_loss_scaled +
             results.kl_loss * results.kl_scale
         )
 
-        # get the latnet usage
+        # get the latent usage
         p_kl = self.running_kls_per_channel / (self.running_kls_per_channel.sum() + 1e-7)
         results.effective_parties = (1 / (p_kl ** 2).sum().item()) / p_kl.numel()
 
         p_mean_kl = self.running_mean_kls_per_channel / (self.running_mean_kls_per_channel.sum() + 1e-7)
         results.effective_parties_mean = (1 / (p_mean_kl ** 2).sum().item()) / p_mean_kl.numel()
+
+        p_zero_kl = self.running_zeros_per_channel / (self.running_zeros_per_channel.sum() + 1e-7)
+        results.effective_parties_zero = (1 / (p_zero_kl ** 2).sum().item()) / p_zero_kl.numel()
 
         if step % self.log_image_interval == 0:
 
@@ -165,15 +182,15 @@ class ZLmFullTrainer(BaseTrainer):
                 mode='L'
             )
 
-            # results.kl_mask = Image(
-            #     (normalized_kl_weights > self.kl_weight_threshold).float().detach().cpu().numpy().reshape(model.z_length, model.num_latent_layers).T,
-            #     mode='L'
-            # )
+            results.kl_mask = Image(
+                final_kl_weights.detach().cpu().numpy().reshape(model.z_length, model.num_latent_layers).T / final_kl_weights.max().item(),
+                mode='L'
+            )
 
-            # results.mean_kl_mask = Image(
-            #     (normalized_mean_kl_weights > self.kl_weight_threshold).float().detach().cpu().numpy().reshape(model.z_length, model.num_latent_layers).T,
-            #     mode='L'
-            # )
+            results.zero_kl_mask = Image(
+                final_zero_kl_weights.cpu().numpy().reshape(model.z_length, model.num_latent_layers).T / final_zero_kl_weights.max().item(),
+                mode='L'
+            )
 
             enc_mus_to_plot = model.shaper.unlayerfy(model_out.encoder_mus).detach()
             gen_mus_to_plot = model.shaper.unlayerfy(model_out.generator_mus).detach()
@@ -197,17 +214,6 @@ class ZLmFullTrainer(BaseTrainer):
                 np.clip(sims.cpu().numpy() / sim_quant, 0.0, 1.0),
                 mode='L'
             )
-
-            z_sims = torch.cdist(
-                model_out.z_scan.detach(),
-                model_out.z_scan.detach(),
-            ).mean(0)
-            z_sim_quant = torch.quantile(z_sims.flatten(), 0.90, dim=-1).item()
-            results.z_sims = Image(
-                np.clip(z_sims.cpu().numpy() / z_sim_quant, 0.0, 1.0),
-                mode='L'
-            )
-
 
         return results
     
